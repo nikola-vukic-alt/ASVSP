1e11f228a0af349d63def01b557967e1393b9ae9
nikola-vukic-alt <vukic.e268.2023@uns.ac.rs>
2024-03-14 15:14:24 +0100

    streaming to kafka

--- streaming/producer/main.py
+++ streaming/producer/main.py
@@ -2,7 +2,8 @@ from os import environ
 from pyspark.sql import SparkSession
 from kafka import KafkaProducer
 import kafka.errors
-import time 
+import time
+from json import dumps
 
 KAFKA_CONFIGURATION = {
     "bootstrap_servers": environ.get("KAFKA_BROKER", "localhost:29092").split(","),
@@ -11,29 +12,29 @@ KAFKA_CONFIGURATION = {
     "reconnect_backoff_ms": int(environ.get("KAFKA_RECONNECT_BACKOFF_MS", "100"))
 }
 
-def get_connection():
-    while True:
-        try:
-            producer = KafkaProducer(**KAFKA_CONFIGURATION)
-            print("Connected to Kafka!")
-            return producer
-        except kafka.errors.NoBrokersAvailable as e:
-            print(e)
-            time.sleep(1)
-
-def producing():
-    producer = get_connection()
-    spark = SparkSession.builder.appName("Streaming Reviews").getOrCreate()
-    spark.sparkContext.setLogLevel("ERROR")
-    
-    df_reviews = spark.read.csv(path="/data/streaming/reviews.csv", header=True, inferSchema=True)
-
-    for row in df_reviews.collect():
+def send_partition_to_kafka(partition):
+    producer = KafkaProducer(**KAFKA_CONFIGURATION)
+    for row in partition:
         key = f"{row['userId']}_{row['movieId']}_{row['timestamp']}"
+        value = row.asDict(True)
         
-        producer.send("reviews-topic", key=str(key), value=row.asDict())
-        print("Sent row to Kafka:", row)
+        print(f"Sending to Kafka - Key: {key}, Value: {value}")
+        
+        producer.send("reviews-topic", key=key, value=value)
         time.sleep(1)
 
+    producer.flush()
+    producer.close()
+
+
+def main():
+    spark = SparkSession.builder.appName("Streaming Reviews to Kafka").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    df_reviews = spark.read.csv("/data/streaming/reviews.csv", header=True, inferSchema=True)
+    df_reviews.foreachPartition(send_partition_to_kafka)
+
 if __name__ == "__main__":
-    producing()
+    try:
+        main()
+    except Exception as e:
+        print(f"An error occurred: {e}")
