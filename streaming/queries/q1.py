3502312933a18ceba266a1a2236f0a2aee831331
nikola-vukic-alt <vukic.e268.2023@uns.ac.rs>
2024-03-15 15:14:46 +0100

    first streaming query done

--- streaming/queries/q1.py
+++ streaming/queries/q1.py
@@ -14,14 +14,14 @@ ELASTIC_SEARCH_PORT = environ.get("ELASTIC_SEARCH_PORT", "9200")
 ELASTIC_SEARCH_INDEX = "streaming_query_1"
 
 def save_data(df, ELASTIC_SEARCH_INDEX):
-    console_query = df \
+    df \
         .writeStream \
         .outputMode("complete") \
         .format("console") \
         .option("truncate", "false") \
         .start()
 
-    es_query = df \
+    df \
         .writeStream \
         .outputMode("append") \
         .option("checkpointLocation", "/tmp/EL_" + ELASTIC_SEARCH_INDEX) \
@@ -34,8 +34,6 @@ def save_data(df, ELASTIC_SEARCH_INDEX):
         .option('es.port', ELASTIC_SEARCH_PORT) \
         .option('es.batch.write.retry.wait', '100s') \
         .start(ELASTIC_SEARCH_INDEX)
-        
-    return console_query, es_query
     
 def quiet_logs(sc):
     logger = sc._jvm.org.apache.log4j
@@ -72,29 +70,15 @@ reviews = reviews.withColumn("value", col("value").cast("string"))
 # Parse the JSON data from the value column
 reviews = reviews.select(from_json(col("value"), schema).alias("review")).select("review.*")
 
-# Group by movieId and calculate the count of reviews for each movieId within the last minute
+# Koji su najviše kritikovani filmovi u prethodnih minut? Azurirano svakih 30 sekundi.
 review_counts = reviews \
     .withWatermark("timestamp", "1 minute") \
     .groupBy(window("timestamp", "1 minute", "30 seconds"), "movieId", "title") \
     .count()
 
-# Order by count of reviews in descending order and limit the output to show only the top movies
 top_movies = review_counts \
-    .limit(10)  # Limit to show top 10 movies
+    .limit(20)  
 
-# Call save_data and get the query objects
-console_query, es_query = save_data(top_movies, ELASTIC_SEARCH_INDEX)
+save_data(top_movies, ELASTIC_SEARCH_INDEX)
 
-# Sort the DataFrame in descending order by the count column
-sorted_top_movies = top_movies.orderBy(col("count").desc())
-
-# Start the streaming query to output the results to the console
-query = sorted_top_movies \
-    .writeStream \
-    .outputMode("complete") \
-    .format("console") \
-    .option("truncate", "false") \
-    .start()
-
-# Wait for the termination of any of the streams
 spark.streams.awaitAnyTermination()
